89c7588ae2e14d76f5a15355a44eb0008971a26a
David Barbarisi <davidbarbarisi@gmail.com>
2015-02-19 15:48:30 -0500

    Updated jrdevsetup version

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ berks update
 berks vendor
 
 cd packer
-packer build -only=vmware-iso centos-6.6-x86_64.json
 packer build -only=virtualbox-iso centos-6.6-x86_64.json
+packer build -only=vmware-iso centos-6.6-x86_64.json
